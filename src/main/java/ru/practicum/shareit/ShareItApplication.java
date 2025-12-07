2a20f29b6e953a2aac951c3a267772aaf0a47064
viktoravramenko <viktoravramenkl@yandex.com>
2025-12-07 14:56:32 +0300

    ФЗ14

---
+++ src/main/java/ru/practicum/shareit/ShareItApplication.java
@@ -0,0 +1,10 @@
+package ru.practicum.shareit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "ru.practicum.shareit")
+public class ShareItApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItApplication.class, args);
+    }
+}
