ea0c925b987951927909098a96bd5fb6e70011c6
viktoravramenko <viktoravramenkl@yandex.com>
2025-12-14 01:31:04 +0300

    postman tests

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -16,7 +16,6 @@ import ru.practicum.shareit.user.UserService;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
