e09300ccc8ae2f47fd160be5aa96f899eb8aadf2
viktoravramenko <viktoravramenkl@yandex.com>
2025-12-22 00:24:39 +0300

    postman problem solved

--- server/src/main/java/ru/practicum/shareit/entity/User.java
+++ server/src/main/java/ru/practicum/shareit/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @Entity
@@ -19,4 +21,26 @@ public class User {
 
     @Column(name = "email", nullable = false)
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
